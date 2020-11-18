f3359d9d2b0b33508043e26d74bc74d478b425ed
osvein <osvein@users.noreply.github.com>
2020-11-18 18:57:03 +0100

    fix register access in driver

--- driver-gamepad-1.0/driver-gamepad.c
+++ driver-gamepad-1.0/driver-gamepad.c
@@ -2,14 +2,18 @@
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/ioport.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-
-#include "efm32gg.h"
+#include <linux/types.h>
 
 /* TODO remember to write in report about gpio subsystem */
 
-char gpio;
+static const phys_addr_t gamepad_portaddr = 0x4006048;
+static const unsigned long gamepad_portsize = 0x24;
+static const unsigned long gamepad_portoff_model = 0x04;
+static const unsigned long gamepad_portoff_dout = 0x0C;
+static const unsigned long gamepad_portoff_din = 0x1C;
 
 /**
  * Opens the gamepad
@@ -22,9 +26,6 @@ char gpio;
 static int gamepad_open(struct inode *inode, struct file *file)
 {
 	if ((file->f_flags & O_ACCMODE) != O_RDONLY) return -EACCES;
-	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO; /* enable GPIO clock */
-	*GPIO_PC_MODEL = 0x33333333; /* input with filter and pull */
-	*GPIO_PC_DOUT = 0xFF; /* pull up */
 	return 0;
 }
 
@@ -42,7 +43,7 @@ static ssize_t gamepad_read(struct file *file, char __user *buf, size_t count,
 	loff_t *off
 ) {
 	if (count < 1) return 0;
-	*buf = *GPIO_PC_DIN;
+	*buf = ioread32(gamepad_portaddr + gamepad_portoff_din);
 	return 1;
 }
 
@@ -73,21 +74,30 @@ static struct cdev gamepad_cdev = {
 };
 
 static struct class *gamepad_class;
+static struct device *gamepad_dev;
+static void __iomem *gamepad_port;
+static struct clk *gamepad_gpioclk;
 
 /**
  * Closes the gamepad and removes it from the device
 */
 static void gamepad_exit(void)
 {
-	dev_t dev;
+	dev_t devno;
 	unsigned count;
 
-	dev = gamepad_cdev.dev;
+	devno = gamepad_cdev.dev;
 	count = gamepad_cdev.count;
-	device_destroy(gamepad_class, dev);
-	class_destroy(gamepad_class);
+	if (gamepad_gpioclk) {
+		clk_disable(gamepad_gpioclk);
+		clk_put(gamepad_gpioclk);
+	}
+	if (gamepad_class) {
+		device_destroy(gamepad_class, devno);
+		class_destroy(gamepad_class);
+	}
 	cdev_del(&gamepad_cdev);
-	unregister_chrdev_region(dev, count);
+	if (count) unregister_chrdev_region(devno, count);
 }
 
 /**
@@ -97,17 +107,43 @@ static void gamepad_exit(void)
 */
 static int __init gamepad_init(void)
 {
-	int ret;
-	dev_t dev;
+	long ret;
+	dev_t devno;
 
 	cdev_init(&gamepad_cdev, &gamepad_fops);
-	ret = alloc_chrdev_region(&dev, 0, 1, "gamepad");
+	ret = alloc_chrdev_region(&devno, 0, 1, "gamepad");
 	if (ret < 0) goto err;
-	ret = cdev_add(&gamepad_cdev, dev, 1);
+	ret = cdev_add(&gamepad_cdev, devno, 1);
 	if (ret < 0) goto err;
-	gamepad_class = class_create(THIS_MODULE, "gamepad");
-	if (IS_ERR(gamepad_class)) goto err;
-	device_create(gamepad_class, NULL, dev, NULL, "gamepad");
+	gamepad_class = class_create(THIS_MODULE, "gamepad"));
+	if (IS_ERR(gamepad_class)) {
+		ret = -PTR_ERR(gamepad_class);
+		goto err;
+	}
+	gamepad_dev = device_create(gamepad_class, NULL, dev, NULL, "gamepad");
+	if (IS_ERR(gamepad_dev)) {
+		ret = -PTR_ERR(gamepad_dev);
+		goto err;
+	}
+
+	if (!devm_request_mem_region(gamepad_dev, gamepad_portaddr,
+		gamepad_portsize, "gamepad_port"
+	)) {
+		ret = -EBUSY;
+		goto err;
+	}
+	gamepad_port = devm_ioremap(gamepad_dev, gamepad_portaddr, gamepad_portsize);
+	if (!gamepad_port) {
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	gamepad_gpioclk = clk_get(NULL, "HFPERCLK.GPIO");
+	if (IS_ERR(gpioclk)) return -PTR_ERR(gpioclk);
+	ret = clk_enable(gpioclk);
+	if (ret < 0) goto err;
+	iowrite32(0x33333333, gamepad_port + gamepad_portoff_model);
+	iowrite32(0xFF, gamepad_port + gamepad_portoff_dout);
 	return 0;
 err:
 	gamepad_exit();
