7ca0f5755c8b3dddaecab9321193d5d2f944d775
Eirik <eirikw@live.no>
2020-11-18 14:20:44 +0100

    to compile

--- game-1.0/game.c
+++ game-1.0/game.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <linux/fb.h>
+#include <unistd.h>
 
 #define WIDTH = 320
 #define HEIGHT = 240
@@ -10,24 +11,6 @@ typedef struct {
 	int x;
 	int y;
 } Pt;
-
-
-
-int main(int argc, char *argv[])
-{
-	uint16_t *buffer_map;
-	int fd;
-
-	fd = open("/dev/fb0", O_RDWR);
-	buffer_map = mmap(NULL, WIDTH*HEIGHT*2, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-	
-	draw_rect({50, 50}, {WIDTH-50, Height-50}, *buffer_map, from_hex(0x00FF00));
-	ioctl(fd, 0x4680, & (struct fb_copyarea){.dx=0, .dy=0, .width=WIDTH, .height=HEIGHT});
-
-	printf("Hello World, I'm game!\n");
-
-	exit(EXIT_SUCCESS);
-}
 
 int idx(Pt point)
 {
@@ -45,9 +28,25 @@ void draw_rect(Pt pt1, Pt pt2, uint16_t *buffer, uint16_t colour){
 }
 
 uint16_t from_hex(unsigned long hex_rgb){
-	unsigned char r = hex_rgb >> 8 & 0b1111100000000000;
-	unsigned char g = hex_rgb >> 5 & 0b0000011111100000;
-	unsigned char b = hex_rgb >> 3 & 0b0000000000011111;
+	unsigned long r = hex_rgb >> 8 & 0b1111100000000000;
+	unsigned long g = hex_rgb >> 5 & 0b0000011111100000;
+	unsigned long b = hex_rgb >> 3 & 0b0000000000011111;
 	return r|g|b;
+}
 
+
+int main(int argc, char *argv[])
+{
+	uint16_t *buffer_map;
+	int fd;
+
+	fd = open("/dev/fb0", O_RDWR);
+	buffer_map = mmap(NULL, WIDTH*HEIGHT*2, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	
+	draw_rect({50, 50}, {WIDTH-50, HEIGHT-50}, *buffer_map, from_hex(0x00FF00));
+	ioctl(fd, 0x4680, & (struct fb_copyarea){.dx=0, .dy=0, .width=WIDTH, .height=HEIGHT});
+
+	printf("Hello World, I'm game!\n");
+
+	exit(EXIT_SUCCESS);
 }
