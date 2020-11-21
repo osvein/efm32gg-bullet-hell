28bbdb064b31fc7df528021e7bd7bad6d4a5add2
osvein <osvein@users.noreply.github.com>
2020-11-21 14:33:12 +0100

    fix speed

--- game-1.0/game.c
+++ game-1.0/game.c
@@ -178,7 +178,7 @@ int main(int argc, char *argv[]) {
 			.player = 0xFFFFFFul,
 			.bullet = 0xFF0000ul
 		},
-		.player = {.speed = 1, .size = 10},
+		.player = {.speed = 10, .size = 10},
 		.draw = {0, 0, dirtylist, lenof(dirtylist)},
 		.bullets = {bullet_pool, bullet_pool, endof(bullet_pool)},
 	};
