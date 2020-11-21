113c2802f736e2c6d999f5c4648a44d6b067831b
osvein <osvein@users.noreply.github.com>
2020-11-21 16:09:26 +0100

    fix

--- game-1.0/draw.c
+++ game-1.0/draw.c
@@ -72,7 +72,10 @@ int draw_map(Draw *self) {
 	{
 		struct fb_var_screeninfo info;
 		if (ioctl(self->fd, FBIOGET_VSCREENINFO, &info) < 0) return -1;
-		self->max = vec_add(draw_upscale(self, (Vec){info.xres, info.yres}), (Vec){-1, -2});
+		self->max = vec_add(
+			draw_upscale(self, (Vec){info.xres, info.yres-1}),
+			(Vec){-1, -1}
+		);
 	}
 	self->buf = mmap(NULL, self->bufsize, PROT_READ|PROT_WRITE, MAP_SHARED,
 		self->fd, 0
