cf1ca752d83841f325dd3a3256e44fbcf5d26737
Sondre Haugen Elgaaen <sondrhel@stud.ntnu.no>
2020-11-21 14:55:55 +0100

    Adjust bullet frequency

--- game-1.0/game.c
+++ game-1.0/game.c
@@ -122,8 +122,9 @@ void game_updateplayer(Game *self, unsigned long delta) {
  * Generates bullets which aim for the player
 */
 void game_gen_target_bullet(Game *self, short speed) {
+	if (self->bullets.end - self->bullets.inactive >= 8) return;
 	Bullet *b = bulletpool_get(&self->bullets);
-	if (!b) return;
+	//if (!b) return;
     b->pos = vec_rand(vec_zero, self->draw.max);
     b->velocity = vec_normalize(
     	vec_add(vec_mul(b->pos, -1), self->player.pos),
@@ -135,9 +136,7 @@ void game_gen_target_bullet(Game *self, short speed) {
  * Generates bullets in a pattern
 */
 void game_gen_pattern_bullets(Game *self, short speed) {
-	if (self->bullets.end - self->bullets.inactive < 30) {
-		return;
-	}
+	if (self->bullets.end - self->bullets.inactive >= 9) return;
 	static Vec angles[] = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
 	int i;
 	for (i = 0; i < 8; i++) {
